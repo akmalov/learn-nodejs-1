20fb3e58d03a716506e951322a6f97ed027d2c71
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-04 13:03:16 +0300

    add code for adding and saving notes

---
+++ playground/json.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+let originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('note.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
