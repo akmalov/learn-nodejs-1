64c587d9790d58846db7a19e2cf08a530f8f6529
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-04 14:49:32 +0300

    configure commands with yargs

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
-let command = process.argv[2];
-console.log('Command: ', command);
-console.log('Yargs', argv);
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    command('list', 'List all notes')
+    command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+let command = argv._[0];
 
 if (command === 'add') {
   let note = notes.addNote(argv.title, argv.body);
@@ -20,7 +40,9 @@ if (command === 'add') {
     console.log('Note title taken');
   }
 } else if (command === 'list') {
-    notes.getAll();
+    let allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
     let note = notes.getNote(argv.title);
     if (note) {
